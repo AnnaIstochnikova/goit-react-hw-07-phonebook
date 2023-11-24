bf2290aab339ee435d9db45674c6ca65889abb71
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-24 13:35:33 +0100

    Optimize Extra Reducer

--- src/redux/usersSlice.js
+++ src/redux/usersSlice.js
@@ -16,43 +16,38 @@ const phonebookSlice = createSlice({
       state.filter = action.payload;
     },
   },
-  extraReducers: builder => {
-    builder
-      .addCase(fetchContacts.pending, state => {
-        state.isLoading = true;
-      })
-      .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.contacts = action.payload;
-        state.error = null;
-      })
-      .addCase(fetchContacts.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
+  extraReducers: {
+    [fetchContacts.pending](state) {
+      state.isLoading = true;
+    },
+    [fetchContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.contacts = action.payload;
+      state.error = null;
+    },
+    [fetchContacts.rejected](state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
 
-      .addCase(deleteContact.fulfilled, (state, action) => {
-        state.contacts = state.contacts.filter(
-          contact => contact.id !== action.payload.id
-        );
-        state.error = null;
-      })
-      .addCase(deleteContact.rejected, (state, action) => {
-        state.error = action.payload;
-      })
-
-      // .addCase(addContacts.pending, (state) => {
-      //   state.isLoading = true;
-      // })
-      .addCase(addContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.contacts.push(action.payload);
-        state.error = null;
-      })
-      .addCase(addContact.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      });
+    [deleteContact.fulfilled](state, action) {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload.id
+      );
+      state.error = null;
+    },
+    [deleteContact.rejected](state, action) {
+      state.error = action.payload;
+    },
+    [addContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.contacts.push(action.payload);
+      state.error = null;
+    },
+    [addContact.rejected](state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
